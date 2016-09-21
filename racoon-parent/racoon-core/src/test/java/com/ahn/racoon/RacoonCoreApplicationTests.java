3785d0e45b195ffe62beda71e902688e2992cbbd
Administrator <Administrator@10.161.99.69>
2016-09-21 15:34:51 -0400

    Project Setup

---
+++ racoon-parent/racoon-core/src/test/java/com/ahn/racoon/RacoonCoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ahn.racoon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RacoonCoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ racoon-parent/racoon-web/src/main/java/com/ahn/racoon/RacoonWebApplication.java
@@ -0,0 +1,12 @@
+package com.ahn.racoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacoonWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RacoonWebApplication.class, args);
+	}
+}
